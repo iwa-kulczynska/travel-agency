b6795af9f082b04b7cd32cf2575c692c1e154545
Iwona Kulczynska <iwa.kulczynska@gmail.com>
2020-11-24 20:00:31 +0100

    fix views for pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+import {AnimatedSwitch} from 'react-router-transition';
 import PropTypes from 'prop-types';
 
 import MainLayout from './components/layout/MainLayout/MainLayout';
@@ -13,6 +14,11 @@ import NotFound from './components/views/NotFound/NotFound';
 
 import parseTrips from './utils/parseTrips';
 import {setMultipleStates} from './redux/globalRedux';
+import Countries from './components/views/Countries/CountriesContainer';
+import Regions from './components/views/Regions/RegionsContainer';
+import Trip from './components/views/Trip/TripContainer';
+import Country from './components/views/Country/CountryContainer';
+import styles from './styles/app.scss';
 
 class App extends React.Component {
   static propTypes = {
@@ -37,13 +43,16 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <MainLayout>
-          <Switch location={location}>
+          <AnimatedSwitch location={location} atEnter={{ opacity: 0 }} atLeave={{ opacity: 0 }} atActive={{ opacity: 1 }} className={styles.switchWrapper} >
             <Route exact path='/' component={Home} />
             <Route exact path='/trips' component={Trips} />
-            {/* TODO - add more routes for other views */}
+            <Route exact path='/trip/:id' component={Trip} />
+            <Route exact path='/countries' component={Countries} />
+            <Route exact path='/country/:id' component={Country} />
+            <Route exact path='/regions' component={Regions} />
             <Route exact path='/info' component={Info} />
             <Route path='*' component={NotFound} />
-          </Switch>
+          </AnimatedSwitch>
         </MainLayout>
       </BrowserRouter>
     );
