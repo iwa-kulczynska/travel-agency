187ae3a85a357e7e6093cdd3b41722a9775a5261
Iwona Kulczynska <iwa.kulczynska@gmail.com>
2020-11-30 18:04:20 +0100

    add single option

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { Row, Col } from 'react-flexbox-grid';
 import OrderSummary from '../../features/OrderSummary/OrderSummary';
 import PropTypes from 'prop-types';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
 const OrderForm = ({tripCost, options}) => (
   <Row>
+    {pricing.map((option) => {
+      return (
+        <Col key={option.id} md={4}>
+          <OrderOption {...option} />
+        </Col>
+      );
+    })}
     <Col xs={12}>
       <OrderSummary tripCost={tripCost} options={options} />
     </Col>
