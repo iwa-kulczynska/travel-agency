3321712a99c878864e36c87d9325dc36a2ae9f5a
Iwona Kulczynska <iwa.kulczynska@gmail.com>
2020-12-07 22:44:53 +0100

    add tests

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -4,12 +4,12 @@ import TripSummary from './TripSummary';
 
 describe('Component TripSummary', () => {
   it('should render without crashing', () => {
-    const component = shallow(<TripSummary id={'abc'} image={'testSource'} name={'testAlt'} cost={'$100'} days={10} tags={[]} />);
+    const component = shallow(<TripSummary id={'abc'} image={'testSource'} name={'testAlt'} cost={'$100'} days={10} />);
     expect(component).toBeTruthy();
   });
 
   it('should render id', () => {
-    const component = shallow(<TripSummary id={'abc'} image={'testSource'} name={'testAlt'} cost={'$100'} days={10} tags={[]} />);
+    const component = shallow(<TripSummary id={'abc'} image={'testSource'} name={'testAlt'} cost={'$100'} days={10} />);
     const expectedHref = '/trip/abc';
     expect(component.find('Link').prop('to')).toEqual(expectedHref);
   });
@@ -21,7 +21,7 @@ describe('Component TripSummary', () => {
   it('image should has correct alt and source', () => {
     const expectedAlt = 'testAlt';
     const expectedSource = 'testSource';
-    const component = shallow(<TripSummary image={'testSource'} name={'testAlt'} cost={'$100'} days={10} tags={[]} />);
+    const component = shallow(<TripSummary image={'testSource'} name={'testAlt'} cost={'$100'} days={10} />);
   
     const renderedImage = component.find('img');
     expect(renderedImage.prop('src')).toEqual(expectedSource);
@@ -32,10 +32,24 @@ describe('Component TripSummary', () => {
     const expectedName = 'testName';
     const expectedCost = 'from $100';
     const expectedDays = '10 days';
-    const component = shallow(<TripSummary name={'testName'} image={'testSource'} id={'abc'} cost={'$100'} days={10} tags={[]} />);
+    const component = shallow(<TripSummary name={'testName'} image={'testSource'} id={'abc'} cost={'$100'} days={10} />);
   
     expect(component.find('h3').text()).toEqual(expectedName);
     expect(component.find('span').first().text()).toEqual(expectedDays);
     expect(component.find('span').at(1).text()).toEqual(expectedCost);
+  });
+
+  it('should render three tags', () => {
+    const expectedTag = 'test1';
+    const component = shallow(<TripSummary tags={['test0','test1','test2']} name={'testName'} image={'testSource'} id={'abc'} cost={'$100'} days={10} />);
+
+    expect(component.find('.tags > span').length).toEqual(3);
+    expect(component.find('.tags > span').at(1).text()).toEqual(expectedTag);
+  });
+  
+  it('should not render div with tags', () => {
+    const component = shallow(<TripSummary name={'testName'} image={'testSource'} id={'abc'} cost={'$100'} days={10} />);
+
+    expect(component.exists('.tags')).toEqual(false);
   });
 });
