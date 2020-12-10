bcf33423b3e306b02979e248a39a995cd8455b74
Iwona Kulczynska <iwa.kulczynska@gmail.com>
2020-12-11 00:13:40 +0100

    add data validation

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -3,9 +3,46 @@ import { Row, Col } from 'react-flexbox-grid';
 import OrderSummary from '../../features/OrderSummary/OrderSummary';
 import PropTypes from 'prop-types';
 import pricing from '../../../data/pricing.json';
+import settings from '../../../data/settings';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import {formatPrice} from '../../../utils/formatPrice';
 
-const OrderForm = ({tripCost, options, setOrderOption}) => (
+const isOrderNowActive = ({name, contact}) => {
+  return (name == null || contact == null);
+};
+
+const sendOrder = (options, tripCost, id, name) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+    id,
+    name,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
+
+const OrderForm = ({tripCost, options, setOrderOption, id, name}) => (
   <Row>
     {pricing.map((option) => {
       return (
@@ -15,6 +52,7 @@ const OrderForm = ({tripCost, options, setOrderOption}) => (
       );
     })}
     <Col xs={12}>
+      <Button disabled={isOrderNowActive(options)} onClick={() => sendOrder(options, tripCost, id, name)}>Order now!</Button>
       <OrderSummary tripCost={tripCost} options={options} />
     </Col>
   </Row>
@@ -25,6 +63,8 @@ OrderForm.propTypes = {
   options: PropTypes.object,
   currentValue: PropTypes.string,
   setOrderOption: PropTypes.func,
+  id: PropTypes.string,
+  name: PropTypes.string,
 };
 
 export default OrderForm;
